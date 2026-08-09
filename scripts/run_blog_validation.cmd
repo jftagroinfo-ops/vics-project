2e81688e79d34e50687bfc632dc620652ac1bdc6
jftagroinfo-ops <jftagro.info@gmail.com>
2026-08-09 22:42:19 +0530

    a

--- scripts/run_blog_validation.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-py -3 scripts\apply_site_audit_fixes.py || exit /b 1
-py -3 scripts\add_image_dimensions.py || exit /b 1
-py -3 scripts\apply_site_audit_fixes.py || exit /b 1
-py -3 scripts\validate_blog_navigation.py || exit /b 1
-py -3 -u scripts\audit_website.py
